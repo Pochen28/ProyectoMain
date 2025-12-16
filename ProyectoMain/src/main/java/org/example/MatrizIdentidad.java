08b306e42a34688b1594ff0d58650cde154ceebb
DESKTOP-DUKR00O\Usuario <rieraljoseluis202@gmail.com>
2025-12-16 14:39:20 +0100

    Continuación

--- ProyectoMain/src/main/java/org/example/MatrizIdentidad.java
+++ ProyectoMain/src/main/java/org/example/MatrizIdentidad.java
@@ -6,11 +6,28 @@ import java.util.Scanner;
 public class MatrizIdentidad {
     static void main() {
         Scanner teclado = new Scanner(System.in);
-        int contador = 0;
+
+        boolean repetir = true;
+        boolean verificar = true;
         int dimensiones = 0;
-        String num = "";
-        System.out.println("Bienvenido a la matriz identidad\nDame las dimensiones de la matriz");
-        dimensiones = teclado.nextInt();
+
+        System.out.println("Bienvenido a la matriz identidad\nDame la dimensión de la matriz");
+        do {
+            try{
+                dimensiones = teclado.nextInt();
+            }catch (Exception Err){
+                repetir = false;
+            }
+
+            if (dimensiones > 1 && dimensiones <= 50){
+                repetir = true;
+            }else {
+                System.out.println("El valor de la dimensión de la matriz debe de ser un número entre 2 y 50, vuelve a introducir la dimensión.");
+            }
+            teclado.nextLine();
+        }while (!repetir);
+
+
         int matriz [] [] = new int[dimensiones][dimensiones];
         System.out.println("Rellena la matriz " + dimensiones + "x" + dimensiones);
         // dimensiona maxima de matriz es 50, y minimo 2
@@ -18,14 +35,31 @@ public class MatrizIdentidad {
         // solicitar
 
         for (int i = 0; i < dimensiones; i++) {
+            for (int j = 0; j < dimensiones; j++) {
+                matriz [i] [j] = teclado.nextInt();
 
-            num = teclado.next();
-            String numstring = num.replace(" ", "");
-            int numint = Integer.parseInt(numstring);
-            matriz[i] [contador] = numint;
-            contador++;
-
+            }
         }
+
+        for (int i = 0; i < dimensiones; i++) {
+            for (int j = 0; j < dimensiones; j++) {
+                if (i == j) {
+                    if (matriz[i][j] != 1) {
+                        verificar = false;
+                    }
+                } else {
+                    if (matriz[i][j] != 0) {
+                        verificar = false;
+                    }
+                }
+            }
+        }
+        if (verificar) {
+            System.out.println("SI");
+        } else {
+            System.out.println("NO");
+        }
+
         for (int columnas []: matriz){
             for (int filas: columnas){
                 System.out.print(filas + " ");
